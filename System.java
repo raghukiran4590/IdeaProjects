83d6b335c995ae7330689100d351d35e6158e503
raghukiran4590 <raghukiran4590@gmail.com>
2024-11-24 17:55:43 -0500

    System.java

---
+++ System.java
@@ -0,0 +1,4 @@
+System.out.println(Runtime.getRuntime().availableProcessors());
+System.out.println(“Total memory available to JVM ”+           Runtime.getRuntime().totalMemory()
+System.out.println(“Total bytes of memory used by JVM : “+Runtime.getRuntime().totalMemory() - Runtime.getRuntime().freeMemory());
+
